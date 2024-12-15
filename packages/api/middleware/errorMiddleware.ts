86396d999a205967d9881bce0cc46df4d8889ead
Mladen Milosavljevic <15332696+DivideByZ3r0@users.noreply.github.com>
2024-12-15 21:09:00 +0100

    feature (api): REfactoring Error Handler

--- packages/api/middleware/errorMiddleware.ts
+++ packages/api/middleware/errorMiddleware.ts
@@ -1,6 +1,15 @@
-function errorHandler(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).json({ error: "Internal Server Error" });
+import { Request, Response, NextFunction } from "express";
+
+const INTERNAL_SERVER_ERROR_MESSAGE = "Internal Server Error";
+
+function errorHandler(
+  error: Error,
+  _: Request,
+  response: Response,
+  _: NextFunction,
+): void {
+  console.error(error.stack);
+  response.status(500).json({ error: INTERNAL_SERVER_ERROR_MESSAGE });
 }
 
 export default errorHandler;
