e8d8b45f4f82c69c67b635f252cccbaf4765a8b9
Mladen Milosavljevic <15332696+DivideByZ3r0@users.noreply.github.com>
2024-12-15 20:55:54 +0100

    feature (api): Added rate limiter from express-rate-limiter

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,6 +1,8 @@
 import express, { Router } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { rateLimit } from "express-rate-limit";
+
 import searchRoutes from "./routes/searchRoutes";
 import hotelsRoutes from "./routes/hotelsRouter";
 import countryRoutes from "./routes/countryRouter";
@@ -9,12 +11,20 @@ import connectDB from "./config/db";
 
 dotenv.config({ path: "./config/.env" });
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 connectDB();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(limiter);
 
 const router = Router();
 
