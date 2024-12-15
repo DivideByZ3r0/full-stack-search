abefe3fc1f6a7482df9ea58d56a85784f02ccf54
Mladen Milosavljevic <15332696+DivideByZ3r0@users.noreply.github.com>
2024-12-15 12:29:39 +0100

    feature (validators): added dto validator with zod

--- packages/api/controllers/searchController.ts
+++ packages/api/controllers/searchController.ts
@@ -1,13 +1,22 @@
 import {Request, Response} from "express";
 import * as searchServices from "../services/searchServices";
+import searchAllDto from '../../types/src/searchAllDto'
+import { z } from "zod";
 
 export const searchHotels = async (req: Request, res: Response) => {
-    const filters = req.body; // Assuming filters are sent in the request body
-
+    const filters = req.body;
     try {
-        const results = await searchServices.searchHotels(filters.searchTerm);
+        const { searchTerm } = searchAllDto.parse(filters);
+        const results = await searchServices.searchHotels(searchTerm);
         res.status(200).json(results);
     } catch (error) {
+        if (error instanceof z.ZodError) {
+            return res.status(400).json({
+                error: "Validation failed",
+                details: error.errors,
+            });
+        }
+
         console.error('[HotelsController]', error);
         res.status(500).json({
             error: 'An unexpected error occurred while searching for hotels.',
